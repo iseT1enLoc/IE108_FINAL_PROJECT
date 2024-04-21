eed1ad01038a0554f3647a21e4b4cdc3fa9b6725
TienLoc <locnvt.it@gmail.com>
2024-04-21 21:16:26 +0700

    fixed

--- test/login_test.dart
+++ test/login_test.dart
@@ -1,78 +1,9 @@
 import 'dart:async';
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
-import 'package:pizza_app/screens/auth/blocs/sign_up_bloc/sign_up_bloc.dart';
 import 'package:pizza_app/screens/auth/blocs/sing_in_bloc/sign_in_bloc.dart';
 import 'package:user_repository/user_repository.dart';
-
-/* class MockFirebaseAuth extends Mock implements FirebaseAuth {}
-
-class MockUserRepo extends Mock implements UserRepository {}
-
-class MockUser extends Mock implements User {}
-
-void main() {
-  group('LoginBloc', () {
-    late MockFirebaseAuth mockFirebaseAuth;
-    late MockUserRepo mock_user_repo;
-    late MockUser mock_user;
-    setUp(() {
-      mockFirebaseAuth = MockFirebaseAuth();
-      mock_user_repo = MockUserRepo();
-      mock_user = MockUser();
-    });
-
-    test('emits LoginProcess and LoginSuccess for successful login', () {
-      final String email = 'test@example.com';
-      final String password = 'password123';
-
-      /*   when(() => mockFirebaseAuth.signInWithEmailAndPassword(
-          email: email,
-          password: password)).thenReturn(Future.value(mock_user)); */
-      when(() => mockFirebaseAuth.signInWithEmailAndPassword(
-          email: email, password: password)).thenAnswer((_) async {
-        return Future.value();
-      });
-      final bloc = SignInBloc(mock_user_repo);
-
-      final expectedStates = [
-        LoginInitial(),
-        LoginProcess(),
-        LoginSuccess(),
-      ];
-
-      expectLater(bloc.stream, emitsInOrder(expectedStates));
-
-      bloc.add(SignInRequired(email, password));
-    });
-
-    test('emits LoginProcess and LoginFailure for login failure', () {
-      final String email = 'test@example.com';
-      final String password = 'wrongpassword';
-      final errorMessage = 'Invalid email or password.';
-
-      when(mockFirebaseAuth.signInWithEmailAndPassword(
-              email: email, password: password))
-          .thenThrow(Exception(errorMessage));
-
-      final bloc = LoginBloc(mockFirebaseAuth);
-
-      final expectedStates = [
-        LoginInitial(),
-        LoginProcess(),
-        LoginFailure(errorMessage: errorMessage),
-      ];
-
-      expectLater(bloc.stream, emitsInOrder(expectedStates));
-
-      bloc.add(LoginButtonPressed(email, password));
-    });
-  });
-}
- */
-import 'package:flutter_test/flutter_test.dart';
 import 'package:bloc_test/bloc_test.dart';
 
 // Your imports and classes
