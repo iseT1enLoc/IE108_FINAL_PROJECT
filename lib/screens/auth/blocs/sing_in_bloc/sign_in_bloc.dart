010d58a76d0ab831d2a2e43555e4cf5a0d9baca4
TienLoc <locnvt.it@gmail.com>
2024-04-21 18:24:21 +0700

    complete SignIn unit test

--- lib/screens/auth/blocs/sing_in_bloc/sign_in_bloc.dart
+++ lib/screens/auth/blocs/sing_in_bloc/sign_in_bloc.dart
@@ -7,19 +7,56 @@ part 'sign_in_state.dart';
 
 class SignInBloc extends Bloc<SignInEvent, SignInState> {
   final UserRepository _userRepository;
-
-  SignInBloc(
-    this._userRepository
-  ) : super(SignInInitial()) {
+  bool _isSignedIn = false;
+  SignInBloc(this._userRepository) : super(SignInInitial()) {
     on<SignInRequired>((event, emit) async {
-      emit(SignInProcess());
+      /* emit(SignInProcess());
       try {
+        emit(SignInSuccess());
         await _userRepository.signIn(event.email, event.password);
+        return;
       } catch (e) {
         emit(SignInFailure());
+      } */
+      if (!_isSignedIn) {
+        // Check if already signed in (optional)
+        emit(SignInProcess());
+        try {
+          await _userRepository.signIn(event.email, event.password);
+          _isSignedIn = true; // Update flag upon successful sign-in (optional)
+          emit(SignInSuccess());
+        } catch (e) {
+          emit(SignInFailure());
+        }
       }
     });
-    
-    on<SignOutRequired>( (event, emit) async => await _userRepository.logOut());
+
+    on<SignOutRequired>((event, emit) async => await _userRepository.logOut());
   }
 }
+/*
+class SignInBloc extends Bloc<SignInEvent, SignInState> {
+  final UserRepository _userRepository;
+  bool _isSignedIn = false;
+
+  SignInBloc(this._userRepository) : super(SignInInitial()) {
+    on<SignInRequired>((event, emit) async {
+      if (!_isSignedIn) {
+        emit(SignInProcess());
+        try {
+          await _userRepository.signIn(event.email, event.password);
+          _isSignedIn = true; // Set flag to true upon successful sign-in
+          emit(SignInSuccess()); // Emit success state after successful sign-in
+        } catch (e) {
+          emit(SignInFailure());
+        }
+      }
+    });
+
+    on<SignOutRequired>((event, emit) async {
+      await _userRepository.logOut();
+      _isSignedIn = false; // Reset flag upon sign-out
+    });
+  }
+}
+*/
